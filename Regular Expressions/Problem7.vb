c3ca7f876db074064d2e4a2c7fa9c9353ccc7c3e
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-09-26 05:00:00 +0100

    2018 Updates

---
+++ Regular Expressions/Problem7.vb
@@ -0,0 +1,13 @@
+﻿'Problem 7: Write a program that counts vowels in a String using Regular Expression.
+Imports System.Text.RegularExpressions
+Public Class WebForm1
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim word As String = "Hello World"
+        Dim pattern As String = "[^aeiouAEIOU]"
+        Dim count As Integer = Regex.Replace(word, pattern, "").Count
+        TextBox1.Text = count
+    End Sub
+
+End Class
