0fa38dab45dbe9200747b24c36a4be17b5776ab2
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-09-26 05:00:00 +0100

    2018 Updates

---
+++ Regular Expressions/Problem3.vb
@@ -0,0 +1,12 @@
+﻿'Problem 3: Write a program that checks if string contains sample format date of (yyyy-mm-dd)
+Imports System.Text.RegularExpressions
+Public Class WebForm1
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim pattern As String = "([0-9]{4})-([0-9]{2})-([0-9]{2})"
+        TextBox1.Text = Regex.IsMatch("2018-01-02", pattern)
+        TextBox2.Text = Regex.IsMatch("01-01-02", pattern)
+    End Sub
+
+End Class
