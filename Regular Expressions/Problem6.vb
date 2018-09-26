8542d66a8eea8d6471fff929483c0f69be4996f7
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-09-26 05:00:00 +0100

    2018 Updates

---
+++ Regular Expressions/Problem6.vb
@@ -0,0 +1,12 @@
+﻿'Problem 6: Write a program that removes white space and non-visible characters.
+Imports System.Text.RegularExpressions
+Public Class WebForm1
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim word As String = "Hello World"
+        Dim pattern As String = "\s"
+        TextBox1.Text = Regex.Replace(word, pattern, "")
+    End Sub
+
+End Class
