04bb263434a973b62c9fca48463df401dddac43b
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-09-26 05:00:00 +0100

    2018 Updates

---
+++ Regular Expressions/Problem5.vb
@@ -0,0 +1,12 @@
+﻿'Problem 5: Write a program that matches time in 24 hour format.
+Imports System.Text.RegularExpressions
+Public Class WebForm1
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim pattern As String = "^(0?[1-9]|1[012])(:[0-5]\d) [APap][mM]$"
+        TextBox1.Text = Regex.IsMatch("25:00 am", pattern)
+        TextBox2.Text = Regex.IsMatch("08:01 am", pattern)
+    End Sub
+
+End Class
