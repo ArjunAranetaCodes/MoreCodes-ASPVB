c89592cb9bf392efa003cc8a8d96a688e49a423b
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-09-26 05:00:00 +0100

    2018 Updates

---
+++ Regular Expressions/Problem10.vb
@@ -0,0 +1,12 @@
+﻿'Problem 10: Write a program that prints an integer with commas separator using Regular Expression.
+Imports System.Text.RegularExpressions
+Public Class WebForm1
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim word As String = "1000"
+        Dim pattern As String = "(\d)(?=(\d{3})+$)"
+        TextBox1.Text = Regex.Replace(word, pattern, "$1,")
+    End Sub
+
+End Class
