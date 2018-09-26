8adecfb5f297924f927c89e42737801192a7a704
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-09-26 05:00:00 +0100

    2018 Updates

---
+++ Regular Expressions/Problem8.vb
@@ -0,0 +1,12 @@
+﻿'Problem 8: Write a program that checks if the String if valid url using Regular Expression.
+Imports System.Text.RegularExpressions
+Public Class WebForm1
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim pattern As String = "^(https?:\/\/)?(www\.)?([\w]+\.)+[‌​\w]{2,63}\/?$"
+        TextBox1.Text = Regex.IsMatch("http://www.example.com", pattern)
+        TextBox2.Text = Regex.IsMatch("wwwexamplecom", pattern)
+    End Sub
+
+End Class
