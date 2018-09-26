e0a4685205f04f8e116f83975545ef506f03bd03
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-09-26 05:00:00 +0100

    2018 Updates

---
+++ Regular Expressions/Problem2.vb
@@ -0,0 +1,12 @@
+﻿'Problem 2: Write a program that matches email address.
+Imports System.Text.RegularExpressions
+Public Class WebForm1
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim pattern As String = "^[_A-Za-z0-9-\+]+(\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\.[A-Za-z0-9]+)*(\.[A-Za-z]{2,})$"
+        TextBox1.Text = Regex.IsMatch("mark@yahoo.com", pattern)
+        TextBox2.Text = Regex.IsMatch("mark-yahoo.com", pattern)
+    End Sub
+
+End Class
