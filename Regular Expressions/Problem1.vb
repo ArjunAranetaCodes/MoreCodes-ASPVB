d16da804aaf561b653337cf003053688faf8c41e
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-09-26 05:00:00 +0100

    2018 Updates

---
+++ Regular Expressions/Problem1.vb
@@ -0,0 +1,12 @@
+﻿'Problem 1: Write a program to test if the first character of the String is uppercase.
+Imports System.Text.RegularExpressions
+Public Class WebForm1
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim pattern As String = "^[A-Z][a-z0-9_-]{3,19}$"
+        TextBox1.Text = Regex.IsMatch("Hello", pattern)
+        TextBox2.Text = Regex.IsMatch("world", pattern)
+    End Sub
+
+End Class
