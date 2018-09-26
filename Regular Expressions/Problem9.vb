4675e67e1422878a016a9474a8632a66fdc6e727
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-09-26 05:00:00 +0100

    2018 Updates

---
+++ Regular Expressions/Problem9.vb
@@ -0,0 +1,12 @@
+﻿'Problem 9: Write a program that checks if the string is alphanumeric using Regular Expression.
+Imports System.Text.RegularExpressions
+Public Class WebForm1
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim pattern As String = "(([A-Z].*[0-9])|([0-9].*[A-Z]))"
+        TextBox1.Text = Regex.IsMatch("HelloWorld", pattern)
+        TextBox2.Text = Regex.IsMatch("HelloWorld123", pattern)
+    End Sub
+
+End Class
