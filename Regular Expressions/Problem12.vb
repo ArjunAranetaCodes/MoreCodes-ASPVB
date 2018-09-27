fc639f8caaa28628f564f08ccf04698fc1a31816
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-09-27 05:00:00 +0100

    2018 Updates

---
+++ Regular Expressions/Problem12.vb
@@ -0,0 +1,12 @@
+﻿'Problem 12: Write a program that counts the occurrence of digits in a string using Regular Expression.
+Imports System.Text.RegularExpressions
+Public Class WebForm1
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim word As String = "Hello12 World12"
+        Dim pattern As String = "\D"
+        TextBox1.Text = Regex.Replace(word, pattern, "").Count
+    End Sub
+
+End Class
