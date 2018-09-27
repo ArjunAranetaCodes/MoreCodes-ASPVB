e0670cebeaa6d124fb739b4859247b8f46f51e98
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-09-27 05:00:00 +0100

    2018 Updates

---
+++ Regular Expressions/Problem19.vb
@@ -0,0 +1,13 @@
+﻿'Problem 19: Write a program that counts all numbers in a string using Regular Expression.
+Imports System.Text.RegularExpressions
+Public Class WebForm1
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim word As String = "Hello World123"
+        Dim pattern As String = "[^0-9]"
+        Dim count As Integer = Regex.Replace(word, pattern, "").Count
+        TextBox1.Text = count
+    End Sub
+
+End Class
