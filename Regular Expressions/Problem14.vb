a2ff3f7fed72ef746c844efbde90b09c61fc9a89
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-09-27 05:00:00 +0100

    2018 Updates

---
+++ Regular Expressions/Problem14.vb
@@ -0,0 +1,13 @@
+﻿'Problem 14: Write a program that recognizes valid hex color value using Regular Expression.
+Imports System.Text.RegularExpressions
+Public Class WebForm1
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim pattern As String = "^([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])\." &
+                     "([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])$"
+        TextBox1.Text = Regex.IsMatch("192.168.1.1", pattern)
+        TextBox2.Text = Regex.IsMatch("1.1.1.1.1", pattern)
+    End Sub
+
+End Class
