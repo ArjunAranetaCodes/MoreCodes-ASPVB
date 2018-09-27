eb6ad883e04b8ad8051eddc6daaf29d9d32325e7
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-09-27 05:00:00 +0100

    2018 Updates

---
+++ Regular Expressions/Problem20.vb
@@ -0,0 +1,12 @@
+﻿'Problem 20: Write a program that validates if string length is between 5 to 10 using Regular Expression.
+Imports System.Text.RegularExpressions
+Public Class WebForm1
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim pattern As String = "\w{5,10}\b"
+        TextBox1.Text = Regex.IsMatch("Hello World", pattern)
+        TextBox2.Text = Regex.IsMatch("Hi", pattern)
+    End Sub
+
+End Class
