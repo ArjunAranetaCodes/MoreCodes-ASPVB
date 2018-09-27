123f431f8bba302986ebcb5f9ea22c9185585af8
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-09-27 05:00:00 +0100

    2018 Updates

---
+++ Regular Expressions/Problem17.vb
@@ -0,0 +1,12 @@
+﻿'Problem 17: Write a program that removes the last word in a string using Regular Expression.
+Imports System.Text.RegularExpressions
+Public Class WebForm1
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim word As String = "Hello World"
+        Dim pattern As String = "\w+$"
+        TextBox1.Text = Regex.Replace(word, pattern, "")
+    End Sub
+
+End Class
