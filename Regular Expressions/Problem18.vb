7a9bbe55166386602e5bd08db8b96c8ae51c3402
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-09-27 05:00:00 +0100

    2018 Updates

---
+++ Regular Expressions/Problem18.vb
@@ -0,0 +1,13 @@
+﻿'Problem 18: Write a program that extracts string inside quotation marks using Regular Expression.
+Imports System.Text.RegularExpressions
+Public Class WebForm1
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim word As String = "Hello 'World'"
+        Dim pattern As String = "'([^""]*)'"
+        Dim extract As String = Regex.Matches(word, pattern).Item(0).ToString
+        TextBox1.Text = extract
+    End Sub
+
+End Class
