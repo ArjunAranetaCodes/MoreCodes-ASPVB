f9d7369f4b18c8c41230657a9f0757bb4f19ebfd
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-09-27 05:00:00 +0100

    2018 Updates

---
+++ Regular Expressions/Problem11.vb
@@ -0,0 +1,13 @@
+﻿'Problem 11: Write a program that counts the occurrence of a string in a string using Regular Expression.
+Imports System.Text.RegularExpressions
+Public Class WebForm1
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim word As String = "HelloWorldHelloWorld"
+        Dim pattern As String = "Hello"
+        Dim count As Integer = Regex.Matches(word, pattern).Count
+        TextBox1.Text = count
+    End Sub
+
+End Class
