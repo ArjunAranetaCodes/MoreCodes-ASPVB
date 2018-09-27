e5481df24d5e64bc13b757b52f134d70c43d4520
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-09-27 05:00:00 +0100

    2018 Updates

---
+++ Regular Expressions/Problem15.vb
@@ -0,0 +1,12 @@
+﻿'Problem 15: Write a program that takes a value inside a <div> tag using Regular Expression.
+Imports System.Text.RegularExpressions
+Public Class WebForm1
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim word As String = "<div>Hello World</div>"
+        Dim pattern As String = "<([^""]*""|'[^']*'|[^'"">])*>"
+        TextBox1.Text = Regex.Replace(word, pattern, "")
+    End Sub
+
+End Class
