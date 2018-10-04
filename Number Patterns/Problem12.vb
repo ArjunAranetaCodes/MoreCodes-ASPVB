3c58b9c6cf6154c6805403fa140794fdec54f8c3
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-10-04 05:00:00 +0100

    2018 Updates

---
+++ Number Patterns/Problem12.vb
@@ -0,0 +1,21 @@
+﻿'Problem 12: Write a program to print the pattern of asterisks using loop.
+'*****
+'****
+'***
+'**
+'*
+Public Class WebForm1
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Label1.Text = ""
+        Dim row As Integer = 4
+        For y As Integer = row To 0 Step -1
+            For x As Integer = 0 To y
+                Label1.Text = Label1.Text & "*"
+            Next
+            Label1.Text = Label1.Text & "<br/>"
+        Next
+    End Sub
+
+End Class
