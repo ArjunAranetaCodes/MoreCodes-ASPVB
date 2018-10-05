932540e0519e17f160a22a5038f85dc9c0204c58
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-10-05 05:00:00 +0100

    2018 Updates

---
+++ Number Patterns/Problem16.vb
@@ -0,0 +1,21 @@
+﻿'Problem 16: Write a program to print the number pattern using loop.
+'12345
+'1234
+'123
+'12
+'1
+Public Class WebForm1
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Label1.Text = ""
+        Dim row As Integer = 4
+        For y As Integer = row To 0 Step -1
+            For x As Integer = 0 To y
+                Label1.Text = Label1.Text & (x + 1)
+            Next
+            Label1.Text = Label1.Text & "<br/>"
+        Next
+    End Sub
+
+End Class
