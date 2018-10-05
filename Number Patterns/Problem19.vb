070c5e5e0f5d16b86fd6089e61acfcf30a4d6128
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-10-05 05:00:00 +0100

    2018 Updates

---
+++ Number Patterns/Problem19.vb
@@ -0,0 +1,28 @@
+﻿'Problem 19: Write a program to print the number pattern using loop.
+'11 11
+'11 11
+
+'11 11
+'11 11
+Public Class WebForm1
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Label1.Text = ""
+        Dim row As Integer = 4
+        Dim col As Integer = 4
+        For y As Integer = 0 To row
+            For x As Integer = 0 To col
+                If Math.Round(col / 2) = x Or Math.Round(row / 2) = y Then
+                    Label1.Text = Label1.Text & "&nbsp;&nbsp;"
+                ElseIf (col Mod 2 = 0 And Math.Round(col / 2) = x) Or (row Mod 2 = 0 And Math.Round(row / 2) = y) Then
+                    Label1.Text = Label1.Text & "&nbsp;&nbsp;"
+                Else
+                    Label1.Text = Label1.Text & "1"
+                End If
+            Next
+            Label1.Text = Label1.Text & "<br/>"
+        Next
+    End Sub
+
+End Class
